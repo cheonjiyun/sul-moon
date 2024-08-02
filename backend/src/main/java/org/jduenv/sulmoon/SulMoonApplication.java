3b3776f413e07599ab642618b03ceee768c45e1a
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-08-02 23:14:04 +0900

    [BE] 🎉 init commit

---
+++ backend/src/main/java/org/jduenv/sulmoon/SulMoonApplication.java
@@ -0,0 +1,13 @@
+package org.jduenv.sulmoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulMoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SulMoonApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/jduenv/sulmoon/SulMoonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jduenv.sulmoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulMoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
