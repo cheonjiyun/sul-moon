e736f3ea3b1f6dcc226339feb9b504250d2ea9c6
천지윤 <70828192+cheonjiyun@users.noreply.github.com>
2024-08-05 23:23:35 +0900

    [FE] 🎨UI: 헤더 마크업

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { CgLogIn } from "react-icons/cg";
+
+export const Header = () => {
+    return (
+        <header className="h-16 flex justify-between items-center">
+            <Link href="/">
+                <Image src="/asset/img/logo.svg" alt="설문로고" width={160} height={66} />
+            </Link>
+            <Link className="p-5" href="/login">
+                <div className="sm lg:hidden">
+                    <CgLogIn color="FFB525" size={32} />
+                </div>
+                <div className="hidden lg:block">로그인</div>
+            </Link>
+        </header>
+    );
+};
